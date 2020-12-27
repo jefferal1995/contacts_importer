f96eae3443e31ee681f85c499681a4b02b0920b4
Jefferson Alvarez <jeffersonalvarezalvarez@gmail.com>
2020-12-26 19:11:15 -0500

    Added general structure in Frontend of the platform

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,9 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+//= require jquery3
+//= require popper
+//= require bootstrap
+
+toastr = require("toastr")
